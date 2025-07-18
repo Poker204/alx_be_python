41131e712c9b16e12b3fef55115a0c4941cb65fa
Lenin Mombe <leninmombe91@gmail.com>
2025-07-18 12:03:16 +0200

    Create convert_hours_to_seconds.py

---
+++ python_introduction/convert_hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Get the number of hours from the user
+hours = int(input("Please enter the number of hours: "))
+
+# Convert hours to seconds
+seconds = hours * 3600
+
+# Output the result
+print(f"{hours} hours is equal to {seconds} seconds.")
